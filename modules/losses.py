fe7b549a85e6d1266c23adbfceb1d00211d5c8dc
SieunPark <sieunpark77@gmail.com>
2021-05-10 16:52:33 +0900

    cycle mse batch

--- modules/losses.py
+++ modules/losses.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.applications.vgg19 import preprocess_input, VGG19
-from modules.resizing import imresize_np
+from modules.resizing import resize_batch
 
 def PixelLossDown(criterion='l1', scale=4):
     """pixel loss"""
     def _PixelLossDown(sr, hr):
-        sr_down = imresize_np(sr, 1 / scale)
-        hr_down = imresize_np(hr, 1 / scale)
+        sr_down = resize_batch(sr, 1 / scale)
+        hr_down = resize_batch(hr, 1 / scale)
         if criterion == 'l1':
             return tf.keras.losses.mean_absolute_error(sr_down, hr_down)
         elif criterion == 'l2':
